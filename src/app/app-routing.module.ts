9950a6c683dfe205172bb2f03a0ee59abac0a8c5
David Martins <davidlopes228@hotmail.com>
2020-11-13 21:23:06 -0300

    Login e redirecionamento OK

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { DashboardComponent } from '../app/dashboard/dashboard.component'
+import { AppComponent } from './app.component';
+import { LoginComponent } from '../app/login/login.component'
+
+const routes: Routes = [
+  {path: '', component: LoginComponent},
+  {path: 'dashboard', component: DashboardComponent},
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ src/environments/environment.ts
@@ -0,0 +1,26 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyBBLFEptd6N6UD5l79_t48o58kuyC1gFfo",
+    authDomain: "login-angular-1e533.firebaseapp.com",
+    databaseURL: "https://login-angular-1e533.firebaseio.com",
+    projectId: "login-angular-1e533",
+    storageBucket: "login-angular-1e533.appspot.com",
+    messagingSenderId: "855631538893",
+    appId: "1:855631538893:web:469d77afac40e8744e9784",
+    measurementId: "G-T5SYM4339M"
+  }
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
